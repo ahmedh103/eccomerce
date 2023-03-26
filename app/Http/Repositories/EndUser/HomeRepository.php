277d7f116ecda0097244e66934bf0308d0af321e
gouda <dev.karimgouda@gmail.com>
2023-03-26 14:45:35 +0200

    AllAds

--- app/Http/Repositories/EndUser/HomeRepository.php
+++ app/Http/Repositories/EndUser/HomeRepository.php
@@ -3,15 +3,18 @@
 namespace App\Http\Repositories\EndUser;
 
 use App\Http\Interfaces\EndUser\HomeInterface;
+use App\Http\Traits\AllAdsTrait;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Application;
 
 class HomeRepository implements HomeInterface
 {
-
+    use AllAdsTrait;
     public function index(): View|Application|Factory|\Illuminate\Contracts\Foundation\Application
     {
-        return view('index');
+        $ads = $this->getAllAds();
+//        dd($ads);
+        return view('index',['ads'=>$ads]);
     }
 }

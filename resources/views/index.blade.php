80c6b67db18071ef09354ffdff9e8269cbe0b90e
Mohamed Ali <79287488+Genius-code@users.noreply.github.com>
2023-03-26 22:32:23 +0200

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -34,66 +34,11 @@
                 @foreach($departments as $department)
 
                 <a href="ad-list-column3.html" class="suggest-card">
-                    <img src="{{ asset($department->image) }}" alt="car">
+                    <img src="{{ asset($department->image) }}" alt="{{$department->name}}">
                     <h6>{{$department->name}}</h6>
                     <p>({{$department->categories->count()}})Category</p>
                 </a>
                 @endforeach
-                <a href="ad-list-column3.html" class="suggest-card">
-                    <img src="images/suggest/furniture.png" alt="furniture">
-                    <h6>furniture</h6>
-                    <p>(4,521) ads</p>
-                </a>
-                <a href="ad-list-column3.html" class="suggest-card">
-                    <img src="images/suggest/properties.png" alt="house">
-                    <h6>properties</h6>
-                    <p>(4,521) ads</p>
-                </a>
-                <a href="ad-list-column3.html" class="suggest-card">
-                    <img src="images/suggest/fashion.png" alt="food">
-                    <h6>fashion</h6>
-                    <p>(4,521) ads</p>
-                </a>
-                <a href="ad-list-column3.html" class="suggest-card">
-                    <img src="images/suggest/electronics.png" alt="cycle">
-                    <h6>electronics</h6>
-                    <p>(4,521) ads</p>
-                </a>
-                <a href="ad-list-column3.html" class="suggest-card">
-                    <img src="images/suggest/hospitality.png" alt="clothes">
-                    <h6>hospitality</h6>
-                    <p>(4,521) ads</p>
-                </a>
-                <a href="ad-list-column3.html" class="suggest-card">
-                    <img src="images/suggest/gadgets.png" alt="computer">
-                    <h6>gadgets</h6>
-                    <p>(4,521) ads</p>
-                </a>
-                <a href="ad-list-column3.html" class="suggest-card">
-                    <img src="images/suggest/education.png" alt="phone">
-                    <h6>education</h6>
-                    <p>(4,521) ads</p>
-                </a>
-                <a href="ad-list-column3.html" class="suggest-card">
-                    <img src="images/suggest/software.png" alt="scooter">
-                    <h6>software</h6>
-                    <p>(4,521) ads</p>
-                </a>
-                <a href="ad-list-column3.html" class="suggest-card">
-                    <img src="images/suggest/food.png" alt="television">
-                    <h6>food</h6>
-                    <p>(4,521) ads</p>
-                </a>
-                <a href="ad-list-column3.html" class="suggest-card">
-                    <img src="images/suggest/services.png" alt="truck">
-                    <h6>services</h6>
-                    <p>(4,521) ads</p>
-                </a>
-                <a href="ad-list-column3.html" class="suggest-card">
-                    <img src="images/suggest/animals.png" alt="pet">
-                    <h6>animals</h6>
-                    <p>(4,521) ads</p>
-                </a>
             </div>
         </div>
     </section>

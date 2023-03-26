fe89beeae3099fa22ccc737813c18a6271c75b2b
ziyadragab <ziyadragab00@gmail.com>
2023-03-27 00:53:00 +0200

    ShowDepartment_updates

--- app/Http/Repositories/EndUser/HomeRepository.php
+++ app/Http/Repositories/EndUser/HomeRepository.php
@@ -3,18 +3,21 @@
 namespace App\Http\Repositories\EndUser;
 
 use App\Http\Interfaces\EndUser\HomeInterface;
-use App\Models\Ads;
+use App\Http\Traits\AllAdsTrait;
+use App\Http\Traits\ShowDepartmentTrait;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Application;
 
 class HomeRepository implements HomeInterface
 {
-
+    use AllAdsTrait , ShowDepartmentTrait;
     public function index(): View|Application|Factory|\Illuminate\Contracts\Foundation\Application
     {
-        $our_Recommend_ads =   Ads::inRandomOrder()->limit(4)->get(['image','description','price']);
-        return view('index',compact('our_Recommend_ads'));
+        $departments = $this->ShowDepartment();
+        $ads = $this->getAllAds(8);
+        $our_Recommend_ads =   $this->getAllAds(4);
+        return view('index',compact(['departments','our_Recommend_ads', 'ads']));
     }
 
 }
